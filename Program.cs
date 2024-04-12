127849eb1d711f827aeba39c18147eb26262205b
alexidi <alex.vyrlan@gmail.com>
2024-04-12 21:24:17 +0300

    Fix added

--- Program.cs
+++ Program.cs
@@ -2,48 +2,48 @@
 //Создаем блок кода для ввода пользователем размерности массива
 Console.Write("Введите количество элементов массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
-string [] stringArray = new string [m];
+string[] stringArray = new string[m];
 
 //Ввод данных в массив.
-void array(string [] stringArray)
+void array(string[] stringArray)
 {
-  for (int i = 0;i<stringArray.Length;i++)
+  for (int i = 0; i < stringArray.Length; i++)
   {
-     Console.WriteLine($"Введите {i+1} элемент массива");
-     stringArray[i] = Console.ReadLine();
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    stringArray[i] = Console.ReadLine();
   }
 }
 //Определяем строки с <=3 символами
-string [] count(string [] stringArray)
+string[] count(string[] stringArray)
 {
   int n = 0;
-  for (int i = 0;i<stringArray.Length;i++)
+  for (int i = 0; i < stringArray.Length; i++)
   {
-    if(stringArray[i].Length <=3)
-    n++;
+    if (stringArray[i].Length <= 3)
+      n++;
   }
   //Записываем найденные строки с <=3 элементами в новый массив
-  string [] rez = new string [n];
+  string[] rez = new string[n];
   int j = 0;
-  for (int i = 0;i<stringArray.Length;i++)
+  for (int i = 0; i < stringArray.Length; i++)
   {
-    if(stringArray[i].Length <=3)
+    if (stringArray[i].Length <= 3)
     {
-        rez[j] = stringArray[i];
-        j++;
+      rez[j] = stringArray[i];
+      j++;
     }
   }
   return rez;
 }
 //Выводим массив со строками с <=3 элементами на экран
-void printA(string [] stringArray)
+void printA(string[] stringArray)
 {
-    Console.Write("[");
-    for (int i = 0;i<stringArray.Length;i++)
-    {
+  Console.Write("[");
+  for (int i = 0; i < stringArray.Length; i++)
+  {
     Console.Write($"‘{stringArray[i]}’ ");
-    }
-    Console.Write("]");
+  }
+  Console.Write("]");
 }
 array(stringArray);
 printA(count(stringArray));
