4a450f71ab0294e235f56867ada7df4290a220eb
alexidi <alex.vyrlan@gmail.com>
2024-04-12 21:32:21 +0300

    Fix added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-//Создаем блок кода для ввода пользователем размерности массива
+//Ввод пользователем размерности массива
 Console.Write("Введите количество элементов массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 string[] stringArray = new string[m];
