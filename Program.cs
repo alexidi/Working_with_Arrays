17c4a62cfd86e35b3816e730c7c96c686dc10c1f
alexidi <alex.vyrlan@gmail.com>
2024-04-12 21:22:10 +0300

    Fix added

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ void array(string [] stringArray)
      stringArray[i] = Console.ReadLine();
   }
 }
-//Определяем массивы с <=3 символами
+//Определяем строки с <=3 символами
 string [] count(string [] stringArray)
 {
   int n = 0;
